e8b1c0adc2dc5a38fa670ac68e92e9dea8906b3e
Pablo Henrique <pablohfernandescosta@gmail.com>
2026-03-18 01:43:00 -0300

    Condicional

--- Pasta Curso/Estrutura Condicional/Aulas/p6/Main2/src/App.java
+++ Pasta Curso/Estrutura Condicional/Aulas/p6/Main2/src/App.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        
+
         int hora;
         System.out.println("Qual a hora? ");
         hora = sc.nextInt();
@@ -11,14 +11,12 @@ public class App {
         if (hora < 12){
             System.out.println("Bom dia! ");
         }
+        else if (hora < 18){
+            System.out.println("Boa tarde! ");
+        }
         else{
-            if (hora < 18){
-                System.out.println("Boa tarde! ");
-            }
-            else{
-                System.out.println("Boa noite! ");
-            }
-        }   
+            System.out.println("Boa noite! ");
+        }
         sc.close();
     }
 }

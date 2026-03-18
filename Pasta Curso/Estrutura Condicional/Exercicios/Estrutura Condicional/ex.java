9a70f1be5d5073450093b0803c90b99e53fd4af2
Pablo Henrique <pablohfernandescosta@gmail.com>
2026-03-18 01:54:17 -0300

    Ajuste

--- Pasta Curso/Estrutura Condicional/Exercicios/Estrutura Condicional/ex.java
+++ Pasta Curso/Estrutura Condicional/Exercicios/Estrutura Condicional/ex.java
@@ -1,14 +1,17 @@
-import java.util.Scanner
+import java.util.Scanner;
 public class ex {
     public static void main(String [] args){
 
-        Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner(System.in);
 
+        int n1 = sc.nextInt();
 
-
-
-
-
+        if (n1 > 0){
+            System.out.printf("O numero %d é positivo! ", n1);
+        }
+        else{
+            System.out.printf("O numero %d é negativo! ", n1);
+        }
 
         sc.close();
     }

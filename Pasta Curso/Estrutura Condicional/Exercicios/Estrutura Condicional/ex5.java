fa8cab3a6557a676db2e67aecab711ec193fa83b
Pablo Henrique <pablohfernandescosta@gmail.com>
2026-03-19 03:10:39 -0300

    ajuste

--- Pasta Curso/Estrutura Condicional/Exercicios/Estrutura Condicional/ex5.java
+++ Pasta Curso/Estrutura Condicional/Exercicios/Estrutura Condicional/ex5.java
@@ -1,32 +1,32 @@
-import java.util.Scanner;
+import java.util.Scanner; //Importando o scanner
 public class ex5 {
     public static void main (String [] args){
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); //Setando o Scanner
 
-        int codigo = sc.nextInt();
+        int codigo = sc.nextInt(); //Variais recebendo inteiro
         int quantidade = sc.nextInt();
 
-        double preco;
+        double preco; //Variavel recebendo numero com virgula
         
         if (codigo == 1){
-            preco = quantidade * 4.0;
+            preco = quantidade * 4.0; // cod 1 retorna o preco dele e quantos quer dando o preço final
         }
         else if (codigo == 2){
-            preco = quantidade * 4.5;
+            preco = quantidade * 4.5; // cod 2 retorna o preco dele e quantos quer dando o preço final
         }
         else if (codigo == 3){
-            preco = quantidade * 5.0;
+            preco = quantidade * 5.0; // cod 3 retorna o preco dele e quantos quer dando o preço final
         }
         else if (codigo == 4){
-            preco = quantidade * 2.0;
+            preco = quantidade * 2.0; // cod 4 retorna o preco dele e quantos quer dando o preço final
         }
         else {
-            preco = 1.5;
+            preco = 1.5; // cod 5 retorna o preco dele e quantos quer dando o preço final
         }
 
-        System.out.printf("Total: R$ %.2f%n", preco);
+        System.out.printf("Total: R$ %.2f%n", preco); //Printando a informação na tela
 
-        sc.close();
+        sc.close(); //Fechando o scanner
     }
     
 }

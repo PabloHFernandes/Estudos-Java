659851492e0fa64a266ae953a2acec21537a83f5
Pablo Henrique <pablohfernandescosta@gmail.com>
2026-03-21 01:35:49 -0300

    Ajustes

--- Pasta Curso/Estrutura Condicional/Aulas/p2/Main3/src/App.java
+++ Pasta Curso/Estrutura Condicional/Aulas/p2/Main3/src/App.java
@@ -1,14 +1,18 @@
+import java.util.Locale;
 import java.util.Scanner;
 public class App {
     public static void main(String[] args){
-
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner (System.in);
 
+        int minutos = sc.nextInt();
 
+        double conta = 50.0;
+        if (minutos > 100) {
+            conta += (minutos - 100) * 2.0; //Conta é igual a conta onde oq utrapassar 100 ele vai pegar e fazer vezes 2 e somar com o valor a ser pago até 100 minutos q é o limite
+        }
 
-
-
-
+        System.out.printf("Valor da compra %.2f%n", conta); 
 
         sc.close();
 

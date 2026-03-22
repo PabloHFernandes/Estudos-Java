4e625f22924eb59d4f3c8c44ad58e9e11a00c4e7
Pablo Henrique <pablohfernandescosta@gmail.com>
2026-03-21 23:11:40 -0300

    Aula

--- Pasta Curso/Estrutura Condicional/Aulas/p4/Main5/src/App.java
+++ Pasta Curso/Estrutura Condicional/Aulas/p4/Main5/src/App.java
@@ -1,10 +1,26 @@
-import java.util.Scanner;
 public class App {
     public static void main(String[] args){
-        Scanner sc = new Scanner (System.in);
+
+        double preco1 = 34.5; //Variavel recebendo um numero com virgula
+        double desconto1; //Variavel recebendo um numero com virgula
+
+        if (preco1 < 20.0){ // se o preço for menor doq 20 o desconto é preco1 vezes 0.1
+            desconto1 = preco1 * 0.1;
+        }
+        else{ // se nao é preco1 vezes 0.05
+            desconto1 = preco1 * 0.05;
+        }
+
+        System.out.println(desconto1); //Printar na tela as informações
+
+        //COM A EXPRESSAO CONDICIONAL TERNARIA FICARIA  
+
+        double preco2 = 34.5; //Variavel recebendo um numero com virgula
+        double desconto2 = (preco2 < 20.0) ? preco2 * 0.1 : preco2 * 0.05; // SINTAXE (condição) ? valor_se_verdadeiro : valor_se_falso 
+        System.out.println(desconto2); //Printar na tela as informações
 
 
-        sc.close();
+
         
     }
 }

1129fb1b965cd1e5e15d966297b1698ed3e324c8
Pablo Henrique <pablohfernandescosta@gmail.com>
2026-03-25 02:25:08 -0300

    Adicionado imagens e aula realizada

--- Pasta Curso/Estrutura Repetitiva/Aulas/p1/Main/src/Main.java
+++ Pasta Curso/Estrutura Repetitiva/Aulas/p1/Main/src/Main.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner (System.in); 
+        int x = sc.nextInt(); 
+
+        int soma = 0; // Variavel soma recebendo 0, pois como a variavel precisa receber algum valor, o 0 é o elemento nulo ent iniciará com 0
+
+        //(REGRA: v - executa e volta, f - pula fora! )
         
-    
+        while (x != 0) { // While = ENQUANTO, deve-se usar quando não se sabe previamente a quantidade de repetições que serão rodadas 
+            soma = soma + x; // Soma vai receber o valor dela + o numero q vier na vez da repetição
+            x = sc.nextInt(); // Aq seria para o usuario inserir um novo valor
+        }
+
+        System.out.println(soma); // Printar a soma dos numeros na tela
+
+        sc.close();
     }
 }
